aa61cc755b955205b89afbcdc3fa475e074240ff
yoshility <yoshie@LAPTOP-FVMH1DBA.localdomain>
2022-10-16 03:59:54 +0900

    asm.c : label array

--- sim/asm.c
+++ sim/asm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // アセンブリのコンマを除去
 char* eliminate_comma(char* line) {
@@ -17,15 +18,22 @@ char* eliminate_comma(char* line) {
 int reg(char *reg) {
     if (strncmp(reg, "a0", 2) == 0) {
         return 1010;
-    } else {
+    } else if (strncmp(reg, "a1", 2) == 0) {
+        return 1011;
+    } else { // todo
         return 11111;
     }
 }
 
-int main() {
-    FILE *in;
-    if ((in = fopen("./test/fib.s", "r"))==NULL){
+int main(int argc, char* argv[]) {
+    FILE *in, *out;
+    if ((in = fopen("./test/fib.s", "r")) == NULL) {
         printf("cannot open file\n");
+        exit(1);
+    }
+    if ((out = fopen("./test/fib.txt", "w")) == NULL) {
+        printf("cannot open file\n");
+        exit(1);
     }
 
     char line[100];
@@ -39,6 +47,29 @@ int main() {
 
     char str[50];
 
+    // 1回目の読みでラベルを探してアドレスを振る
+    int addr = -4;
+    char label[1000][10]; // ラベル保管庫
+
+    while (fgets(line, 100, in) != NULL) {
+        addr += 4;
+
+        strcpy(r0, "\0");
+        strcpy(r1, "\0");
+        strcpy(r2, "\0");
+        inst = eliminate_comma(line);
+        res = sscanf(inst, "%s%s%s%s", opcode, r0, r1, r2);
+        // 命令アドレスを出力
+        printf("%02d %s %s %s %s\n", addr, opcode, r0, r1, r2);
+
+        if (opcode[strlen(opcode)-1] == ':') {
+            // 配列のaddr番目にラベル名を保管
+            strcpy(label[addr], opcode);
+            addr -= 4;
+        }
+    }
+
+    // 2回目の読みでバイナリに変換
     while (fgets(line, 100, in) != NULL) {
         strcpy(r0, "\0");
         strcpy(r1, "\0");
@@ -53,7 +84,13 @@ int main() {
             int rs1 = reg(r1);
             long long int imm = 1; // テキトー
             sprintf(str, "%012lld%05d%03d%05d%07d", imm, rs1, 0, rd, 10011);
-            printf("%s\n", str);
+            // fprintf(out, "%s\n", str);
+        }
+        // bge
+        else if (strncmp(opcode, "bge", 3) == 0) {
+            int rs1 = reg(r0);
+            int rs2 = reg(r1);
+            // pcが飛ぶためのbit数が足りないとき
         }
     }
 
