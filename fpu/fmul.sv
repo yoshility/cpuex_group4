7e5aecfe2f19e9a581987537ad3e98f36688ea28
atsumaru1377 <62948930+atsumaru1377@users.noreply.github.com>
2022-10-28 18:26:50 +0900

    fmul.sv

--- fpu/fmul.sv
+++ fpu/fmul.sv
@@ -1,5 +1,31 @@
 `default_nettype none
 
+module fmul (
+  input wire [31:0]  x1,
+  input wire [31:0]  x2,
+  output wire [31:0] y,
+  //output wire        ovf,
+  input wire       clk,
+  input wire       rstn
+);
+	assign sign = x1[31] ^ x2[31];
+	assign exp_sum = x1[30:23] + x2[30:23] + 129;
+	assign exp_add1 = exp_sum + 1;
+	wire [12:0] x1_h, x2_h;
+	wire [10:0] x1_l, x2_l;
+	assign {x1_h, x1_l} = {1'b1, x1[22:0]};
+	assign {x2_h, x2_l} = {1'b1, x2[22:0]};
+	wire [25:0] hh;
+	wire [23:0] hl,lh;
+	assign hh = x1_h * x2_h;
+	assign hl = x1_h * x2_l;
+	assign lh = x1_l * x2_h;
+	wire [25:0] fraction = hh + hl>>11 + lh>>11 + 2;
+	assign y =
+		(~(|x1 || |x2)) ? 32'b0 s:
+		(fraction[25] == 1) ? {sign, exp_add1, fraction[24:2]} :
+		{sign, exp_sum, fraction[23:1]} ;
+endmodule
 
 
 module fmul_step1 (
