1cacd0dfb544512498c49df09622825bd462517f
yoshility <yoshie@LAPTOP-FVMH1DBA.localdomain>
2022-10-30 23:55:25 +0900

    test.cpp: proto cache

--- sim/simmulator/test.cpp
+++ sim/simmulator/test.cpp
@@ -1,34 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 命令形式(type)
-#define R 0
-#define I 1
-#define S 2
-#define B 3
-#define U 4
-#define J 5
-
-int op_regRS1(int ir, int type) {
-    int res = 0;
-    switch(type) {
-        case R: res = (ir >> 15) & 0x1f;
-                break;
-        case I: res = (ir >> 15) & 0x1f;
-                break;
-        case S: res = (ir >> 15) & 0x1f;
-                break;
-        case B: res = (ir >> 15) & 0x1f;
-                break;
-        default: cout << "This instr doesn't have RS1!" << endl;
-                 break;
-    }
-    return res;
-}
+class Cache {
+	private:
+		unsigned int* tag;
+	public:
+		unsigned long long accessed_times;
+		unsigned long long hit_times;
+		unsigned long long miss_times;
+		Cache() {
+			// ライン数4
+			tag = (unsigned int*)malloc(sizeof(unsigned int) * 4);
+		}
+		void read(unsigned int);
+		void write(unsigned int);
+};
 
 int main() {
-	int ir = 0b11111110000011111000111110000000;
-	cout << op_regRS1(ir, 6) << endl;
+	Cache cache;
+	cache.greet();
 	return 0;
 }
 
