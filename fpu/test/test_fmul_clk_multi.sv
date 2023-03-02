9dd690194045b59dd1b4cbd8030536b467a6e993
atsumaru1377 <62948930+atsumaru1377@users.noreply.github.com>
2023-03-02 20:46:15 +0900

    modify testfmul

--- fpu/test/test_fmul_clk_multi.sv
+++ fpu/test/test_fmul_clk_multi.sv
@@ -2,14 +2,19 @@
 `default_nettype wire
 
 module test_fmul(
-   input sys_clk,
-   output correct,
-   output [2:0] latency
+   //board
+   //input logic sys_clk,
+   //output logic  correct,
+   //output logic [2:0] latency
 );
-   clk_wiz_0 clk_gen(
-      .clk_in1(sys_clk),
-      .clk_out1(clk)
-   );
+   logic clk;
+   // sim
+   logic correct;
+   logic [2:0] latency;
+   always begin clk <= 1'b1; #50; clk <= 1'b0; #50; end
+
+   //board
+   //clk_wiz_0 clk_gen(.clk_in1(sys_clk),.clk_out1(clk));
 
    always @(posedge clk) begin
       if (~rstn) begin
@@ -25,10 +30,9 @@ module test_fmul(
    logic diff_gt, diff_lt, diff;
    initial begin
       rstn = 1'b0;
-      #200
+      #100
       rstn = 1'b1;
       repeat (1024) begin
-         latency = 4'b0
          x1 = $random();
          x2 = $random();
          x1_real = $bitstoshortreal(x1);
@@ -40,7 +44,7 @@ module test_fmul(
             diff_lt = sys_y - my_y;
             diff = (diff_gt < diff_lt) ? diff_gt : diff_lt;
             correct = (diff < 2) ? 1'b1 : 1'b0;
-            #200
+            #100;
          end
       end
    end
